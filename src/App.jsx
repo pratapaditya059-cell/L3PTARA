85051005af1e5b13b91408d6670c03eba76059a8
pratapaditya059-cell <pratapaditya059@gmail.com>
2026-07-08 12:33:23 +0530

    Add starry sky as background

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import { Canvas } from "@react-three/fiber";
 import World from "./world/World";
-import { OrbitControls, Sky } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 
 function App() {
   return (
     <Canvas camera={{ position: [6, 5, 6] }}>
-      <Sky
-  sunPosition={[100, 20, 100]}/>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 10, 5]} intensity={1.5} />
-      <World />
-      <OrbitControls />
-    </Canvas>
+  <color attach="background" args={["#050510"]} />
+
+  <Stars
+    radius={100}
+    depth={50}
+    count={5000}
+    factor={4}
+    saturation={0}
+    fade
+    speed={1}
+  />
+
+  <ambientLight intensity={0.4} />
+
+  <directionalLight
+    position={[5, 10, 5]}
+    intensity={1}
+    color="#b8c7ff"
+  />
+
+  <World />
+
+  <OrbitControls />
+</Canvas>
   );
 }
 
